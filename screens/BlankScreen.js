091da95c84071bde89b0e9f40965b846715e9889
Youssef Henna <58384527+YoussefHenna@users.noreply.github.com>
2023-08-31 20:28:07 +0300

    remove actions

--- screens/BlankScreen.js
+++ screens/BlankScreen.js
@@ -2,8 +2,6 @@ import React from 'react';
 import * as GlobalStyles from '../GlobalStyles.js';
 import Breakpoints from '../utils/Breakpoints';
 import * as StyleSheet from '../utils/StyleSheet';
-import getLocationUtil from '../utils/getLocation';
-import openImagePickerUtil from '../utils/openImagePicker';
 import { Button, ScreenContainer, withTheme } from '@draftbit/ui';
 import { useWindowDimensions } from 'react-native';
 
@@ -15,22 +13,6 @@ const BlankScreen = props => {
   return (
     <ScreenContainer hasSafeArea={true} scrollable={false}>
       <Button
-        onPress={() => {
-          const handler = async () => {
-            try {
-              await openImagePickerUtil({
-                mediaTypes: 'All',
-                allowsEditing: false,
-                quality: 0.2,
-              });
-
-              await getLocationUtil();
-            } catch (err) {
-              console.error(err);
-            }
-          };
-          handler();
-        }}
         style={StyleSheet.applyWidth(
           GlobalStyles.ButtonStyles(theme)['Button'],
           dimensions.width
--- utils/index.js
+++ utils/index.js
@@ -1,6 +1,4 @@
 export { default as parseBoolean } from './parseBoolean';
-export { default as openImagePicker } from './openImagePicker';
-export { default as getLocation } from './getLocation';
 export { default as CustomCodeErrorBoundary } from './CustomCodeErrorBoundary';
 export { default as usePrevious } from './usePrevious';
 export { default as useIsOnline } from './useIsOnline';
