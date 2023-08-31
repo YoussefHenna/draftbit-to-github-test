ee5b3de61a2e8a77c52e2bdd2c75b46a42a828f1
Youssef Henna <58384527+YoussefHenna@users.noreply.github.com>
2023-08-31 14:54:26 +0300

    add image picker

---
+++ GlobalStyles.js
@@ -0,0 +1,14 @@
+import * as StyleSheet from './utils/StyleSheet';
+
+import Breakpoints from './utils/Breakpoints';
+
+export const ButtonStyles = theme =>
+  StyleSheet.create({
+    Button: {
+      backgroundColor: theme.colors.primary,
+      borderRadius: 8,
+      fontFamily: 'System',
+      fontWeight: '700',
+      textAlign: 'center',
+    },
+  });
