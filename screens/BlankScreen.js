4c39c8557c3610fbb96fdc62c5b8e7e87841976c
Youssef Henna <58384527+YoussefHenna@users.noreply.github.com>
2023-08-31 20:26:43 +0300

    with actions

--- screens/BlankScreen.js
+++ screens/BlankScreen.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { ScreenContainer, withTheme } from '@draftbit/ui';
+import * as GlobalStyles from '../GlobalStyles.js';
+import Breakpoints from '../utils/Breakpoints';
+import * as StyleSheet from '../utils/StyleSheet';
+import getLocationUtil from '../utils/getLocation';
+import openImagePickerUtil from '../utils/openImagePicker';
+import { Button, ScreenContainer, withTheme } from '@draftbit/ui';
 import { useWindowDimensions } from 'react-native';
 
 const BlankScreen = props => {
@@ -7,7 +12,33 @@ const BlankScreen = props => {
 
   const { theme } = props;
 
-  return <ScreenContainer hasSafeArea={true} scrollable={false} />;
+  return (
+    <ScreenContainer hasSafeArea={true} scrollable={false}>
+      <Button
+        onPress={() => {
+          const handler = async () => {
+            try {
+              await openImagePickerUtil({
+                mediaTypes: 'All',
+                allowsEditing: false,
+                quality: 0.2,
+              });
+
+              await getLocationUtil();
+            } catch (err) {
+              console.error(err);
+            }
+          };
+          handler();
+        }}
+        style={StyleSheet.applyWidth(
+          GlobalStyles.ButtonStyles(theme)['Button'],
+          dimensions.width
+        )}
+        title={'Get Started'}
+      />
+    </ScreenContainer>
+  );
 };
 
 export default withTheme(BlankScreen);
--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,6 @@
 export { default as parseBoolean } from './parseBoolean';
+export { default as openImagePicker } from './openImagePicker';
+export { default as getLocation } from './getLocation';
 export { default as CustomCodeErrorBoundary } from './CustomCodeErrorBoundary';
 export { default as usePrevious } from './usePrevious';
 export { default as useIsOnline } from './useIsOnline';
