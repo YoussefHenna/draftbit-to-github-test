23644e0422a20f6771b6c721c20ac2db77e1f476
Youssef Henna <58384527+YoussefHenna@users.noreply.github.com>
2023-08-31 20:13:48 +0300

    tests

--- screens/BlankScreen.js
+++ screens/BlankScreen.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import * as GlobalStyles from '../GlobalStyles.js';
-import Breakpoints from '../utils/Breakpoints';
-import * as StyleSheet from '../utils/StyleSheet';
-import openImagePickerUtil from '../utils/openImagePicker';
-import { Button, ScreenContainer, withTheme } from '@draftbit/ui';
+import { ScreenContainer, withTheme } from '@draftbit/ui';
 import { useWindowDimensions } from 'react-native';
 
 const BlankScreen = props => {
@@ -11,31 +7,7 @@ const BlankScreen = props => {
 
   const { theme } = props;
 
-  return (
-    <ScreenContainer hasSafeArea={true} scrollable={false}>
-      <Button
-        onPress={() => {
-          const handler = async () => {
-            try {
-              await openImagePickerUtil({
-                mediaTypes: 'All',
-                allowsEditing: false,
-                quality: 0.2,
-              });
-            } catch (err) {
-              console.error(err);
-            }
-          };
-          handler();
-        }}
-        style={StyleSheet.applyWidth(
-          GlobalStyles.ButtonStyles(theme)['Button'],
-          dimensions.width
-        )}
-        title={'Get Started'}
-      />
-    </ScreenContainer>
-  );
+  return <ScreenContainer hasSafeArea={true} scrollable={false} />;
 };
 
 export default withTheme(BlankScreen);
